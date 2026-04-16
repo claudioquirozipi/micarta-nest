2c196c3cca76f17266c064ca4c6615317ec821ba
Claudio Quiroz <claudioquirozipi@gmail.com>
2026-04-16 17:36:05 -0500

    update auth module

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,27 +1,19 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from '../../generated/prisma/client';
+import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
-export class PrismaService implements OnModuleInit, OnModuleDestroy {
-  private readonly client: PrismaClient;
-
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   constructor() {
     const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
-    this.client = new PrismaClient({ adapter } as any);
-  }
-
-  get user() { return this.client.user; }
-
-  async $transaction<T>(fn: (tx: PrismaClient) => Promise<T>): Promise<T> {
-    return this.client.$transaction(fn as any) as Promise<T>;
+    super({ adapter });
   }
 
   async onModuleInit() {
-    await (this.client as any).$connect();
+    await this.$connect();
   }
 
   async onModuleDestroy() {
-    await (this.client as any).$disconnect();
+    await this.$disconnect();
   }
 }
